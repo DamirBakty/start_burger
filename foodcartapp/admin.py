924292ab4a615094aa089e66fbf4c1972bfa9928
Your Name <you@example.com>
2024-05-20 15:24:24 +0500

    add response change

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product, Order, OrderProduct
 from .models import ProductCategory
@@ -132,3 +134,9 @@ class OrderAdmin(admin.ModelAdmin):
             instance.save()
         formset.save_m2m()
 
+    def response_change(self, request, obj):
+        res = super(OrderAdmin, self).response_change(request, obj)
+        next_url = request.GET.get('next', '')
+        if next_url and url_has_allowed_host_and_scheme(url=next_url, allowed_hosts={request.get_host()}):
+            return HttpResponseRedirect(next_url)
+        return res
